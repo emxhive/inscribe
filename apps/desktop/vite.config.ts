a90ddf77cf794f48f11a608fb5739f3c051aa2bb
Zeki <67089614+emxhive@users.noreply.github.com>
2026-01-10 20:14:07 +0100

    Update vite.config.ts

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      '@inscribe/shared': path.resolve(__dirname, '../../packages/shared/src/index.ts'),
+
     },
   },
   server: {
